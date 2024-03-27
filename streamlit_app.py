a17fac02b203db2ef9b034f703c1373a5e4bbb0b
yeyoung <162390239+Jyeyoung@users.noreply.github.com>
2024-03-27 01:03:05 +0000

    hello

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,20 @@
+
 import streamlit as st
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x*x)
+st.title("this is the app title")
+st.header("this is the markdown")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r'''a+ar^1+ar^2+ar^3''')
+
+st.checkbox('yes')
+st.button('Click')
+st.radio
+st.radio('Pick your agender',['Male','Fmale'])
+st.selectbox('choose a planer',['Jupiter','Mars','neptune'])
+st.select_slider('Pick a mark',['Bad','Good','Excellent'])
+st.slider('Pick a number',0,50)
+         
